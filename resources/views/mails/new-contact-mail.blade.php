8b99fdc9dfc7a6afb37ca66ebfa00f477e0487ce
SimoneNegrisoli <simo.negrisoli@gmail.com>
2024-01-24 11:44:02 +0100

    lead controller

---
+++ resources/views/mails/new-contact-mail.blade.php
@@ -0,0 +1,10 @@
+<div>
+    <h2>ciao Admin</h2>
+    <p>
+        Hai ricevuto un messaggio, ecco i dettagli: <br>
+        Nome:{{ $lead->name }}<br>
+        Email:{{ $lead->email }}<br>
+        Address:{{ $lead->address }}<br>
+        Message:{{ $lead->message }}
+    </p>
+</div>
